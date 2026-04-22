7adcd358a5a38fc05da6a378295693bd458de25e
Taha Tariq <tahatariqf@gmail.com>
2026-04-22 04:48:16 -0700

    Model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -46,4 +47,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    model_trainer.initiate_model_training(train_arr, test_arr)
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,8 @@ import dill
 
 from src.exception import CustomException
 
+from sklearn.metrics import r2_score
+
 def save_object(file_path, obj):
     try:
         dir_path  = os.path.dirname(file_path)
@@ -13,5 +15,20 @@ def save_object(file_path, obj):
 
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj,file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
+
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train,y_train)
+            train_pred = model.predict(X_train)
+            test_pred = model.predict(X_test)
+            training_r2score = r2_score(y_train, train_pred)    
+            testing_r2score = r2_score(y_test, test_pred)
+            report[list(models.keys())[i]] = testing_r2score
+        return report  
     except Exception as e:
         raise CustomException(e,sys)
